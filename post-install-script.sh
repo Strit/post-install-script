64ca186bf1dfc8bc83892dc232903d74c6e4702e
Strit <danjohansen@strits.dk>
2014-05-09 16:49:32 +0200

    Code cleanup
    Added git gui to installed programs.
    Removed the setting of username and scrollbars, as that is done with tweak tool which is also installed.

--- post-install-script.sh
+++ post-install-script.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#Version 1.2
-#This script will download about 343 MB's of data, install 26 (27 if on a laptop) programs and remove 4.
-#Tested on Ubuntu 14.04 64-bit (daily build).
+#Version 1.3
+#This script will download about 340 MB's of data, install 26 (27 if on a laptop) programs and remove 5.
+#Tested on Ubuntu 14.04 64-bit (final release).
 
 
 echo "Adding Repositories and installing programs.
 Please type your password!"
-
-#enable username on panel
-gsettings set com.canonical.indicator.session show-real-name-on-panel true
-
-#get regular scrollbars
-gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
 #stop Nautilus
 killall nautilus
@@ -25,24 +19,17 @@ sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
 sudo apt-get update
 
 #install programs
-sudo apt-get -y install audacity simplescreenrecorder compizconfig-settings-manager vlc conky virtualbox unity-tweak-tool griffith handbrake filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl clementine pidgin asunder icedtea-7-plugin openjdk-7-jre vuze wine1.7
+sudo apt-get -y install audacity simplescreenrecorder compizconfig-settings-manager vlc conky virtualbox unity-tweak-tool griffith handbrake filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui clementine pidgin asunder icedtea-7-plugin openjdk-7-jre vuze wine1.7
 
 #uninstall programs
 sudo apt-get -y autoremove rhythmbox empathy firefox totem transmission-common transmission-gtk
 
-#these are disabled to test if they are needed when Ubuntu is installed with 3rd party software
-#gstreamer0.10-plugins-ugly gstreamer0.10-ffmpeg libxine1-ffmpeg gxine mencoder libdvdread4 totem-mozilla icedax tagtool id3tool lame nautilus-script-audio-convert libmad0 mpg321
-
-#Checking if the computer is a laptop and installing TLP if it is (not ready for Trusty yet, using Saucy package)
+#Checking if the computer is a laptop and installing TLP if it is
 if [ -d /sys/class/power_supply/BAT0 ]
 then
 sudo add-apt-repository -y ppa:linrunner/tlp
 sudo apt-get update
 sudo apt-get -y install tlp tlp-rdw
-#wget https://launchpad.net/~linrunner/+archive/tlp/+files/tlp_0.4-1~saucy_all.deb https://launchpad.net/~linrunner/+archive/tlp/+files/tlp-rdw_0.4-1~saucy_all.deb
-#sudo dpkg -i *.deb
-#sudo apt-get -f -y install
-#rm *.deb
 sudo service tlp start
 fi
 
@@ -87,6 +74,7 @@ sudo sh -c 'echo "allow-guest=false" >> /usr/share/lightdm/lightdm.conf.d/50-ubu
 #start Nautilus again
 nautilus &
 
+#tell user what programs where installed and which where uninstalled
 echo "Programs installed succesfully:
 Handbrake (not in Saucy yet, installed Raring package instead. Might be in Trusty's default repo)
 Audacity
@@ -102,6 +90,7 @@ Flash Player
 EasyTag
 Restricted Extras
 Youtube-dl
+Git Gui
 Clementine
 Java Plugin
 Chrome
@@ -109,7 +98,6 @@ Dropbox 1.6
 Steam
 Wine 1.7
 Video and DVD plugins"
-
 if [ -d /sys/class/power_supply/BAT0 ]
 then echo "TLP power management (not ready for Trusty yet, using Saucy package)"
 fi
