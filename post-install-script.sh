08c80208142e5b6e8b2e0f200130ef39314da560
Strit <strit83@gmail.com>
2015-06-26 18:57:12 +0200

    Git test

--- post-install-script.sh
+++ post-install-script.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-#This is a test!
-
-#Version 1.5
+#Version 1.6
 #This script will download between 85 MB and 660 MB's of data depending on your OS.
-#Tested on Ubuntu 14.10, Lubuntu 14.10, Xubuntu 14.10, Kubuntu 14.10, Linux Mint 17, Manjaro Linux and Elementary OS Freya.
+#Tested on Ubuntu 15.04, Linux Mint 17.1, Manjaro Linux, Arch Linux and Elementary OS Freya.
 
 #Defining variables
 os=$(cat /etc/issue | grep '\n' | awk '{print $1,$2}')
 
 #checking os
-if [[ "$os" == "elementary OS" ]]
+if [[ $os == "elementary OS" ]]
 	then
 	echo "Found Elementary OS"
-		elif [[ "$os" == "Linux Mint" ]]
+		elif [[ $os == "Linux Mint" ]]
 		then
 		echo "Found Linux Mint"
-			elif [[ "$os" == "Manjaro Linux" ]]
+			elif [[ $os == "Manjaro Linux" ]]
 			then
-			echo "Found Manajaro Linux"
-				elif [ -d /usr/share/lubuntu ]
+			echo "Found Manjaro Linux"
+				elif [[ $os == "Arch Linux" ]]
 				then
-				echo "Found Lubuntu"
-					elif [ -d /usr/share/xubuntu ]
-					then
-					echo "Found Xubuntu"
-						elif [ -d /usr/share/kubuntu-default-settings ]
-						then
-						echo "Found Kubuntu"
-	else
-	echo "Found Ubuntu"
+				echo "Found Arch Linux"
+					else
+echo "Found Ubuntu"
 fi
 
 if [[ $os == "Manjaro Linux" ]]
@@ -38,49 +30,62 @@ if [[ $os == "Manjaro Linux" ]]
 	echo "Please type your password!"
 	sudo pacman -Syu
 	yaourt -Syu
-		else
+	elif [[ $os == "Arch Linux" ]]
+		then
+		echo "Installing programs."
+		echo "Please type your password"
+		sudo pacman -Syu
+		sudo pacman -S wget
+		mkdir ~/tmp-packages
+		cd ~/tmp-packages
+		wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+		tar -xvf package-query.tar.gz
+		cd package-query
+		makepkg -s
+		sudo pacman -U ~/tmp-packages/package-query/package-query*pkg.tar.xz
+		cd ..
+		wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+		tar -xvf yaourt.tar.gz
+		cd yaourt
+		makepkg -s
+		sudo pacman -U ~/tmp-packages/yaourt/yaourt*pkg.tar.xz
+		cd ..
+		rm -rf ~/tmp-packages/
+		yaourt -Syu
+			else
+echo "Adding Repositories and installing programs."
+echo "Please type your password!"
 
-		echo "Adding Repositories and installing programs."
-		echo "Please type your password!"
+#adding repo's
+sudo add-apt-repository -y ppa:ubuntu-wine/ppa
 
-		#adding repo's
-		sudo add-apt-repository -y ppa:ubuntu-wine/ppa
-
-		#Update repo list and current software
-		sudo apt-get update
-		sudo apt-get -y dist-upgrade
+#Update repo list and current software
+sudo apt-get update
+sudo apt-get -y dist-upgrade
 fi
 
 #install programs
-#There is a bug right now, keeping IcedTea-7-plugin to install properly on 14.10 based OS's.
 if [[ $os == "elementary OS" ]]
 	then
 	echo "Installing extra packages to Elementary OS"
-	sudo apt-get -y install audacity firefox kazam geany thunderbird vlc conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine pidgin steam asunder deluge wine1.7 libreoffice libreoffice-gtk handbrake
+	sudo apt-get -y install audacity chromium-browser simplescreenrecorder geany thunderbird vlc conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine pidgin steam asunder deluge wine1.7 libreoffice libreoffice-gtk handbrake
 		elif [[ $os == "Linux Mint" ]]
 		then
 		echo "Installing extra packages to Linux Mint"
-		sudo apt-get -y install audacity kazam geany conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine steam asunder deluge wine1.7 handbrake
+		sudo apt-get -y install audacity chromium-browser simplescreenrecorder geany conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine steam asunder deluge wine1.7 handbrake
 			elif [[ $os == "Manjaro Linux" ]]
 			then
 			echo "Installing extra packages to Manjaro Linux"
-			sudo pacman -S --noconfirm audacity baobab virtualbox geany conky filezilla easytag youtube-dl clementine asunder deluge wine handbrake evince
-			yaourt -S --noconfirm dropbox griffith kazam
-				elif [ -d /usr/share/lubuntu ]
+			sudo pacman -S --noconfirm audacity chromium virtualbox dropbox simplescreenrecorder geany audacious conky filezilla easytag youtube-dl clementine asunder deluge wine wine_gecko wine-mono handbrake evince baobab soundconverter vorbis-tools hddtemp gnu-netcat
+			yaourt -Sa --noconfirm teamviewer9 griffith magicassistant
+				elif [[ $os == "Arch Linux" ]]
 				then
-				echo "Installing extra packages to Lubuntu"
-				sudo apt-get -y install audacity geany kazam thunderbird vlc conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine steam asunder deluge wine1.7 libreoffice libreoffice-gtk handbrake
-					elif [ -d /usr/share/xubuntu ]
-					then
-					echo "Installing extra packages to Xubuntu"
-					sudo apt-get -y install audacity kazam geany vlc conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine steam asunder deluge wine1.7 libreoffice libreoffice-gtk handbrake
-						elif [ -d /usr/share/kubuntu-default-settings ]
-						then
-						echo "Installing extra packages to Kubuntu"
-						sudo apt-get -y install audacity kazam geany vlc conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine steam asunder deluge handbrake
-	else
-	echo "Installing extra packages to Ubuntu"
-	sudo apt-get -y install audacity kazam geany compizconfig-settings-manager unity-tweak-tool vlc conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine pidgin steam asunder deluge wine1.7 handbrake
+				echo "Installing extra packages to Arch Linux"
+				sudo pacman -S --noconfirm audacity chromium virtualbox simplescreenrecorder geany audacious conky filezilla easytag youtube-dl clementine asunder deluge wine wine_gecko wine-mono handbrake evince baobab soundconverter vorbis-tools hddtemp gnu-netcat
+				yaourt -Sa --noconfirm teamviewer9 pamac-aur dropbox griffith
+					else
+echo "Installing extra packages to Ubuntu"
+sudo apt-get -y install audacity chromium-browser simplescreenrecorder geany compizconfig-settings-manager unity-tweak-tool vlc conky virtualbox griffith filezilla gksu flashplugin-installer easytag curl ubuntu-restricted-extras youtube-dl git-gui hddtemp lm-sensors clementine pidgin steam asunder deluge wine1.7 handbrake
 fi
 
 #uninstall programs
@@ -91,26 +96,17 @@ if [[ $os == "elementary OS" ]]
 		elif [[ $os == "Linux Mint" ]]
 		then
 		echo "Removing unwanted packages from Linux Mint"
-		sudo apt-get -y autoremove transmission-common transmission-gtk banshee totem 
+		sudo apt-get -y autoremove transmission-common transmission-gtk banshee totem firefox
 			elif [[ $os == "Manjaro Linux" ]]
 			then
 			echo "Removing unwanted packages from Manjaro Linux"
-			sudo pacman -Rs --noconfirm gimp xnoise raktpdf
-				elif [ -d /usr/share/lubuntu ]
+			sudo pacman -Rs --noconfirm guayadeque qpdfview firefox-i18n-da firefox
+				elif [[ $os == "Arch Linux" ]]
 				then
-				echo "Removing unwanted packages from Lubuntu"
-				sudo apt-get -y autoremove leafpad audacious transmission-common transmission-gtk sylpheed abiword gnumeric gnome-mplayer
-					elif [ -d /usr/share/xubuntu ]
-					then
-					echo "Removing unwanted packages from Xubuntu"
-					sudo apt-get -y autoremove transmission-common transmission-gtk gmusicbrowser parole abiword gnumeric mousepad 
-						elif [ -d /usr/share/kubuntu-default-settings ]
-						then
-						echo "Removing inwanted packages from Kubuntu"
-						sudo apt-get -y autoremove amarok ktorrent kmail kaddressbook kmix dragonplayer kmousetool kate knotes
-	else
-	echo "Removing unwanted packages from Ubuntu"
-	sudo apt-get -y autoremove rhythmbox empathy totem transmission-common transmission-gtk
+				echo "No unwanted packages on Arch Linux"
+					else
+echo "Removing unwanted packages from Ubuntu"
+sudo apt-get -y autoremove rhythmbox empathy totem transmission-common transmission-gtk firefox
 fi
 
 #making temporary download folder
@@ -118,8 +114,15 @@ mkdir /tmp/script-files/
 
 if [[ $os == "Manjaro Linux" ]]
 	then
-	echo "Manjaro Linux has Laptop Mode Tools installed by default, so will not install TLP."
-	else
+	echo "Manjaro already has battery saving applications installed."
+	elif [[ $os == "Arch Linux" ]]
+		then
+		if [ -d /sys/class/power_supply/BAT* ]
+			then
+			echo "Installing TLP power management"
+			sudo pacman -S tlp
+				else
+	echo "Battery not found. Will not install TLP"
 	#Checking if the computer is a laptop and installing TLP if it is
 	if [ -d /sys/class/power_supply/BAT* ]
 		then
@@ -129,10 +132,11 @@ if [[ $os == "Manjaro Linux" ]]
 		sudo apt-get -y install tlp
 		sudo service tlp start
 		else
-		echo "Battery not found. Will not install TLP"
+	echo "Battery not found. Will not install TLP"
 	fi
+fi
 
-#Checking OS architecture
+	#Checking OS architecture
 	if [ $MACHTYPE = x86_64-pc-linux-gnu ]
 		then
 
@@ -140,8 +144,8 @@ if [[ $os == "Manjaro Linux" ]]
 		wget -P /tmp/script-files/ https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_1.6.2_amd64.deb
 
 		else
-		#Getting install file for Dropbox 32-bit
-		wget -P /tmp/script-files/ https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_1.6.2_i386.deb
+	#Getting install file for Dropbox 32-bit
+	wget -P /tmp/script-files/ https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_1.6.2_i386.deb
 	fi
 
 	#Installing packages
@@ -159,21 +163,45 @@ sudo mv /tmp/script-files/validators.py /usr/share/griffith/lib/db/validators.py
 wget -P /tmp/script-files/ http://www.strits.dk/files/PluginExportPDF.py
 sudo mv /tmp/script-files/PluginExportPDF.py /usr/share/griffith/lib/plugins/export/PluginExportPDF.py
 
+if [[ $os == "Manjaro Linux" ]] || [[ $os == "Arch Linux" ]]
+	then
+	echo " "
+	else
 #fix griffiths IMDB plugin
 wget -P /tmp/script-files/ http://www.strits.dk/files/PluginMovieIMDB.py
 sudo mv /tmp/script-files/PluginMovieIMDB.py /usr/share/griffith/lib/plugins/movie/PluginMovieIMDB.py
+fi
 
-if [[ $os == "Manjaro Linux" ]]
+if [[ $os == "Manjaro Linux" ]] || [[ $os == "Arch Linux" ]]
 	then
-	echo "DVD playback is already enabled in Manjaro Linux, remember to setup hddtemp!"
+	echo "DVD playback is already enabled in Manjaro Linux."
+	sudo systemctl --system daemon-reload
+	sudo systemctl enable hddtemp
+	sudo systemctl start hddtemp
+	sudo systemctl enable teamviewerd
+	sudo systemctl start teamviewerd
 	else
-	#enable DVD playback
-	sudo /usr/share/doc/libdvdread4/install-css.sh
+#enable DVD playback
+sudo /usr/share/doc/libdvdread4/install-css.sh
 
-	#enable hddtemp to run as daemon
-	sudo sed -i 's/RUN_DAEMON="false"/RUN_DAEMON="true"/g' /etc/default/hddtemp
+#enable hddtemp to run as daemon
+sudo sed -i 's/RUN_DAEMON="false"/RUN_DAEMON="true"/g' /etc/default/hddtemp
+fi
+
+if [ -d /sys/class/power_supply/BAT* ]
+	then
+	echo "Applying Conky script optimized for laptops."
+	wget -P /tmp/script-files/ www.strits.dk/files/backup/conkyrc_laptop
+	cp /tmp/script-files/conkyrc_laptop ~/.conkyrc
+
+	else
+echo "Applying Conky script for desktop PC."
+wget -P /tmp/script-files/ www.strits.dk/files/backup/conkyrc_desktop
+cp /tmp/script-files/conkyrc_desktop ~/.conkyrc
 fi
 
 #deleting temporary download folder
 echo "Cleaning up after script..."
 rm -rf /tmp/script-files/
+
+echo "Script completed!"
